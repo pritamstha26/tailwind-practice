f8e770a9453016aba552331a9eba0faa1876c4d6
pritamstha <pritamstha26@gmail.com>
2025-09-03 23:29:11 +0545

    simple change

--- src/component/course/Analytics.jsx
+++ src/component/course/Analytics.jsx
@@ -7,7 +7,7 @@ export default function Analytics() {
         <div className='grid grid-cols-1    md:grid-cols-2 max-w-[1240px] mx-auto'>
                      <img src={img1} alt="" className='w-[500px] mx-auto' />
                  <div>
-                    <p className='text-green-500 text-lg font-extrabold uppercase'>Lorem ipsum dolor sit.</p>
+                    <p className='text-green-500 text-lg font-extrabold uppercase'>Lorem sipsum dolor sit.</p>
                     <p className='sm:text-3xl md:text-4xl  font-extrabold'>Lorem ipsum dolor sit amet consectetur.</p>
                     <p className='text-medium font-bold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, odio.</p>
                     <button className='bg-black text-green-300 px-4 py-2 rounded mx-auto'>Get Started</button>
